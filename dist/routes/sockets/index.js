abbf5d05cc153dc3fbaad8df672499089178e31e
Robert Lombardo <robert@wheelwell.com>
2019-06-19 03:07:06 +0900

    yap

--- dist/routes/sockets/index.js
+++ dist/routes/sockets/index.js
@@ -34,8 +34,7 @@ exports.init = function (app) {
   // console.log(process.env)
 
 
-  var PORT = 80; // process.env.PORT || 3333
-
+  var PORT = process.env.PORT || 3333;
   server.once("listening", function () {
     console.info("\nMultiplayer Bubble Pop - Sockets server is listening on port: ".concat(PORT));
   });
